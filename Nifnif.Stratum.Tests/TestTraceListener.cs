8b8502e4b2395cf24f997d223aea6a628cb2930b
OZZY <OZZY@hashpump.com>
2018-10-08 00:31:27 -0400

    Latest changes

---
+++ Nifnif.Stratum.Tests/TestTraceListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Nifnif.Stratum.Tests
+{
+    public class TestTraceListener : TraceListener
+    {
+        public string WrittenValue {get;set;}
+
+        public override void Write(string message)
+        {
+            WrittenValue = message;
+        }
+
+        public override void WriteLine(string message)
+        {
+            WrittenValue = message;
+        }
+    }
+}
